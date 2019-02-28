d7bf16f3c73bbc94a2f28caba570c60d6a044ba7
I-amCapitalT <37254142+I_amCapitalT@users.noreply.github.com>
2019-02-28 15:57:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const tokenholder = require("./tokenholder.json");
+const tokenholder = process.env.token
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
